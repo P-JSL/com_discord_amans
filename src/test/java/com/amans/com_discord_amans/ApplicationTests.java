c89d0d9a686edede1169d3781e154fb2227fd273
P-JSL <althsuwpfl@gmail.com>
2021-12-17 18:55:15 +0900

    Initial commit

---
+++ src/test/java/com/amans/com_discord_amans/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amans.com_discord_amans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
